3e177948eff7c73a86eb252da9998d55c16082fa
Agustin Torres <agustintorres@gmail.com>
2017-06-29 17:36:04 -0400

    Use close and range

--- equivalent-binary-trees.go
+++ equivalent-binary-trees.go
@@ -6,28 +6,55 @@ import "fmt"
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
 func Walk(t *tree.Tree, ch chan int) { // in-order traversal
+	WalkRecursive(t, ch)
+	close(ch)
+}
+
+func WalkRecursive(t *tree.Tree, ch chan int) { // in-order traversal
 	if t == nil {
 		return
 	}
-	Walk(t.Left, ch)
+	WalkRecursive(t.Left, ch)
 	ch <- t.Value
-	Walk(t.Right, ch)
+	WalkRecursive(t.Right, ch)
 }
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
-	return false
+	ch1 := make(chan int, 10)
+	ch2 := make(chan int, 10)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	
+	for {
+        v1,ok1 := <- ch1
+        v2,ok2 := <- ch2
+
+        if v1 != v2 || ok1 != ok2 {
+            return false
+        }
+
+        if !ok1 {
+            break
+        }
+    }
+	return true
 }
 
 func WalkTest() {
 	ch := make(chan int, 10)
 	go Walk(tree.New(1), ch)
-	for i:= 0; i < cap(ch); i++ {
-		fmt.Println(<-ch)
+	for i := range ch {
+		fmt.Println(i)
 	}
 }
 
 func main() {
 	WalkTest()
+	result := Same(tree.New(1), tree.New(1)) 
+	fmt.Println(result)
+	
+	result = Same(tree.New(1), tree.New(2)) 
+	fmt.Println(result)
 }
