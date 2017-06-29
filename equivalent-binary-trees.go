56ab8a94d845e8c598175240ed2e97ffe545a539
Agustin Torres <agustintorres@gmail.com>
2017-06-29 17:40:06 -0400

    Fix indentation

--- equivalent-binary-trees.go
+++ equivalent-binary-trees.go
@@ -6,28 +6,28 @@ import "fmt"
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
 func Walk(t *tree.Tree, ch chan int) { // in-order traversal
-	WalkRecursive(t, ch)
-	close(ch)
+    WalkRecursive(t, ch)
+    close(ch)
 }
 
 func WalkRecursive(t *tree.Tree, ch chan int) { // in-order traversal
-	if t == nil {
-		return
-	}
-	WalkRecursive(t.Left, ch)
-	ch <- t.Value
-	WalkRecursive(t.Right, ch)
+    if t == nil {
+        return
+    }
+    WalkRecursive(t.Left, ch)
+    ch <- t.Value
+    WalkRecursive(t.Right, ch)
 }
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
-	ch1 := make(chan int, 10)
-	ch2 := make(chan int, 10)
-	go Walk(t1, ch1)
-	go Walk(t2, ch2)
+    ch1 := make(chan int, 10)
+    ch2 := make(chan int, 10)
+    go Walk(t1, ch1)
+    go Walk(t2, ch2)
 	
-	for {
+    for {
         v1,ok1 := <- ch1
         v2,ok2 := <- ch2
 
@@ -39,22 +39,22 @@ func Same(t1, t2 *tree.Tree) bool {
             break
         }
     }
-	return true
+    return true
 }
 
 func WalkTest() {
-	ch := make(chan int, 10)
-	go Walk(tree.New(1), ch)
-	for i := range ch {
-		fmt.Println(i)
-	}
+    ch := make(chan int, 10)
+    go Walk(tree.New(1), ch)
+    for i := range ch {
+	fmt.Println(i)
+    }
 }
 
 func main() {
-	WalkTest()
-	result := Same(tree.New(1), tree.New(1)) 
-	fmt.Println(result)
+    WalkTest()
+    result := Same(tree.New(1), tree.New(1)) 
+    fmt.Println(result)
 	
-	result = Same(tree.New(1), tree.New(2)) 
-	fmt.Println(result)
+    result = Same(tree.New(1), tree.New(2)) 
+    fmt.Println(result)
 }
